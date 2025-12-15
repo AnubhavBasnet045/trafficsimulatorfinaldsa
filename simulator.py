d65944fae4322c7bba23866675629e0dd16d89e5
Anubhav Basnet <anubhavbasnet045@gmail.com>
2025-12-15 20:08:58 +0545

    lane

---
+++ simulator.py
@@ -0,0 +1,34 @@
+from collections import deque
+#----BACKEND---------
+
+class lane:
+     def __init__(self, name, direction):
+          self.name=name
+          self.queue=deque()
+          self.direction= direction
+
+          def add_vehicle(self):
+               self.queue.append(1)
+def serve(self, count):
+        for _ in range(min(count, len(self.queue))):
+            self.queue.popleft()
+
+            def size(self):
+                 return len(self.queue)
+            
+
+class TrafficController:
+     def __init__(self):
+          self.lanes={
+               "AL2": Lane("AL2","down"),
+               "BL2": Lane("BL2", "up"),
+               "CL2": Lane("Cl2", "left"),
+               "Dl2": Lane("Dl2", "right"),
+          }
+
+          self.current_green=None
+
+          
+                
+
+
